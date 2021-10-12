e39839500a9b3dfde30704c5d144d6a62ca8f8b0
Lara <75126217+little-evil-genius@users.noreply.github.com>
2021-10-12 19:43:54 +0200

    Create education.lang.php

---
+++ inc/languages/deutsch_du/education.lang.php
@@ -0,0 +1,51 @@
+<?php
+$l['education'] = "Bildungseinrichtungen";
+$l['education_desc'] = "Beschreibung";
+$l['education_member'] = "Schüler & Schülerinnen";
+
+// FILTER
+$l['education_filter'] = "Filtern nach:";
+$l['education_filter_option_type'] = "Bildungseinrichtungstyp auswählen";
+$l['education_filter_option_city'] = "Stadt auswählen";
+$l['education_filter_button'] = "Filtern";
+
+// BEARBEITUNG & HINZUFÜGEN
+$l['education_add'] = "Bildungseinrichtung hinzufügen";
+$l['education_add_name'] = "Name der Bildungseinrichtung";
+$l['education_add_description'] = "Beschreibung der Bildungseinrichtung";
+$l['education_add_city'] = "Stadt";
+$l['education_add_type'] = "Einrichtungstyp";
+$l['education_add_send'] = "Bildungseinrichtung hinzufügen";
+
+$l['education_edit'] = "Bildungseinrichtung bearbeiten";
+$l['education_edit_send'] = "Bildungseinrichtung editieren";
+
+// BEITRETEN
+$l['education_join'] = "Klasse/Studiengang eintragen";
+$l['education_join_class'] = "Klasse / Studiengang";
+$l['education_join_type'] = "Bildungseinrichtung";
+$l['education_join_send'] = "Klasse/Studiengang eintragen";
+
+// MODCP
+$l['education_modcp'] = "Neue Bilundungseinrichtungen";
+$l['education_modcp_nav'] = "Bilundungseinrichtungen";
+$l['education_modcp_createdby'] = "eingesendet von:";
+$l['education_modcp_accepted'] = "Bilundungseinrichtung annehmen";
+$l['education_modcp_declined'] = "Bilundungseinrichtung ablehnen";
+
+// WEITERLEITUNGS-SEITEN
+$l['education_delete_redirect'] = "Die Bildungseinrichtung wurde erfolgreich gelöscht! Du wirst nun zurückgeleitet.";
+$l['education_add_redirect'] = "Eine neue Bildungseinrichtung wurde erfolgreich hinzugefügt! Du wirst nun zurückgeleitet.";
+$l['education_join_redirect'] = "Deine Klasse/Studiengang wurde erfolgreich eingetragen! Du wirst nun zurückgeleitet.";
+$l['education_edit_redirect'] = "Die Bildungseinrichtung wurde erfolgreich bearbeitet! Du wirst nun zurückgeleitet.";
+$l['education_modcp_declined_redirect'] = "Die Bildungseinrichtung wurde erfolgreich abgelehnt und gelöscht! Du wirst nun zurückgeleitet.";
+$l['education_modcp_accepted_redirect'] = "Die Bildungseinrichtung wurde erfolgreich angenommen! Du wirst nun zurückgeleitet.";
+
+// MyALERTS STUFF
+$l['education_declined'] = '{1} hat deine Bildungseinrichtung <strong>"{4}"</strong> in der Kategorie <strong>"{5}"</strong> und in der Stadt <strong>"{6}"</strong> abgelehnt!';
+$l['myalerts_setting_education_declined'] = "Benachrichtigung bei Ablehnung vom Team bei hinzugefügten Clubs?";
+
+$l['education_accepted'] = '{1} hat deine Bildungseinrichtung <strong>"{4}"</strong> in der Kategorie <strong>"{5}"</strong> und in der Stadt <strong>"{6}"</strong> angenommen!';
+$l['myalerts_setting_education_accepted'] = "Benachrichtigung bei Annahme vom Team bei hinzugefügten Bildungseinrichtungen?";
+
+?>
